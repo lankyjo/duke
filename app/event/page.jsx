370ad367c67b28a555298c892eb48296a3138e20
lankyjo <ikejijoshua69@gmail.com>
2025-02-17 12:35:59 -0500

    blackpage

--- app/event/page.jsx
+++ app/event/page.jsx
@@ -1,7 +1,25 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
 export default function EventPage(){
     return(
-        <div className="py-52 px-5">
-        <h1>Events page</h1>
+        <> 
+        <Header/>
+        <div className="py-44 px-5 bg-[url('/event.webp')] bg-cover bg-center relative">
+        <div className="absolute inset-0 bg-black/50 backdrop-blur-lg"></div>
+
+            <div className="relative space-y-10 text-center">
+            <div className="w-full border border-white aspect-square max-w-[500px] mx-auto ">
+            <img src="/event.webp" alt="image" className="object-cover w-full h-full" />
         </div>
+        <button className="inline-block mx-auto text-ivory bg-black py-5 px-10 text-center">
+            <a href="/purchase" className="block">Buy Ticket</a>
+        </button>
+
+            </div>
+        </div>
+        <Footer/>
+        </>
+
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
 import Concert from "@/components/Concert";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
+      <Header/>
     <Hero/>
     <section className="">
     <Concert/>
     </section>
+    <Footer/>
     </>
   );
 }
